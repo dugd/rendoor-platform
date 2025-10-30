a9ab84049d0502e90e7872fb964976ff60494529
Dugd <dugd.nitkin@gmail.com>
2025-10-30 11:03:29 +0200

    feat: add name field

--- core/domain/user/filter.py
+++ core/domain/user/filter.py
@@ -6,6 +6,7 @@ from .value import PriceFilter, LocationFilter, ApartmentFilter
 class Filter:
     __slots__ = (
         "_uuid",
+        "_name",
         "_user_id",
         "_location_filter",
         "_price_filter",
@@ -15,6 +16,7 @@ class Filter:
     def __init__(
         self,
         user_id: UUID,
+        name: str,
         location_filter: LocationFilter,
         *,
         _uuid: UUID | None = None,
@@ -22,6 +24,7 @@ class Filter:
         apartment_filter: ApartmentFilter | None = None,
     ):
         self._uuid = _uuid or uuid4()
+        self._name = name
         self._user_id = user_id
         self._location_filter = location_filter
         self._price_filter = price_filter
@@ -34,6 +37,10 @@ class Filter:
     @property
     def user_id(self) -> UUID:
         return self._user_id
+
+    @property
+    def name(self) -> str:
+        return self._name
 
     @property
     def location_filter(self) -> LocationFilter:
